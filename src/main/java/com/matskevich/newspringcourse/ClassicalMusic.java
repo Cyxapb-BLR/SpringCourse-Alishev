92f5ca2f25d06f9e6e2f539d83d4c2029fd1b513
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-04 16:44:37 -0500

    L9 - Bean Lifecycle - init() & destroy()

--- src/main/java/com/matskevich/newspringcourse/ClassicalMusic.java
+++ src/main/java/com/matskevich/newspringcourse/ClassicalMusic.java
@@ -1,6 +1,14 @@
 package com.matskevich.newspringcourse;
 
 public class ClassicalMusic implements Music {
+    public void doMyInit() {
+        System.out.println("doing my initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("doing my destruction");
+    }
+
     public String getSong() {
         return "Hungarian Rhapsody";
     }
--- src/main/java/com/matskevich/newspringcourse/TestSpring.java
+++ src/main/java/com/matskevich/newspringcourse/TestSpring.java
@@ -7,24 +7,22 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
 
-       /* Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);*/
-
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        ClassicalMusic musicBean = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(musicBean.getSong());
+        /*MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         boolean comparison = firstMusicPlayer == secondMusicPlayer;
 
         System.out.println(comparison);
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
+
         System.out.println(firstMusicPlayer.getVolume());
         System.out.println(secondMusicPlayer.getVolume());
 
         firstMusicPlayer.setVolume(10);
 
         System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());*/
 
         context.close();
     }
