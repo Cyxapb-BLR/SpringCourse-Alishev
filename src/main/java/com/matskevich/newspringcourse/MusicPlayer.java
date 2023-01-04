a0dce57b02d0320ba1b2e3a55244c4b209e2adf0
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-04 14:28:01 -0500

    HW7 - created musicList, added beans and dependency injection with bean list in xml

--- src/main/java/com/matskevich/newspringcourse/MusicPlayer.java
+++ src/main/java/com/matskevich/newspringcourse/MusicPlayer.java
@@ -1,9 +1,16 @@
 package com.matskevich.newspringcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<Music>();
     private String name;
     private int volume;
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
 
     public String getName() {
         return name;
@@ -21,18 +28,13 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
     public MusicPlayer() {
     }
 
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList
+        ) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
