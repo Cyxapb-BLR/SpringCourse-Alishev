0c763200eb90f394cb078d2c9e5e6840b7ed39d7
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-03 19:04:40 -0500

    first com

---
+++ src/main/java/com/matskevich/newspringcourse/TestSpring.java
@@ -0,0 +1,16 @@
+package com.matskevich.newspringcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml");
+
+        TestBean bean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(bean);
+
+        context.close();
+    }
+}
