cfafd3e2a801103c946adc87a8d3e8f0219e3306
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-05 14:39:01 -0500

    L11 - refactoring for interface Music

--- src/main/java/com/matskevich/newspringcourse/MusicPlayer.java
+++ src/main/java/com/matskevich/newspringcourse/MusicPlayer.java
@@ -5,19 +5,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-
-
-    public MusicPlayer(ClassicalMusic classicalMusic) {
-        this.classicalMusic = classicalMusic;
-    }
+    private Music music;
 
     @Autowired
-    public void setClassicalMusic(ClassicalMusic classicalMusic) {
-        this.classicalMusic = classicalMusic;
+    public MusicPlayer(Music music) {
+        this.music = music;
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + classicalMusic.getSong());
+        System.out.println("Playing: " + music  .getSong());
     }
 }
