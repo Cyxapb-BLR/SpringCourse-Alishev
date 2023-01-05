632b32c0a83d44731120409a26156c893d8e3bd2
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-05 16:02:12 -0500

    HW12-1 - added Lists for music in ClassicalMusic and RockMusic

--- src/main/java/com/matskevich/newspringcourse/ClassicalMusic.java
+++ src/main/java/com/matskevich/newspringcourse/ClassicalMusic.java
@@ -2,10 +2,20 @@ package com.matskevich.newspringcourse;
 
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class ClassicalMusic implements Music {
+    List<String> musicList;
+
+    public ClassicalMusic(List<String> musicList) {
+        this.musicList = musicList;
+        musicList.add("Mozart – Eine kleine Nachtmusik");
+        musicList.add("Beethoven – Für Elise");
+        musicList.add("J.S. Bach – Toccata and Fugue in D minor");
+    }
 
     public String getSong() {
-        return "Hungarian Rhapsody";
+        return musicList.get(2);
     }
 }
--- src/main/java/com/matskevich/newspringcourse/RockMusic.java
+++ src/main/java/com/matskevich/newspringcourse/RockMusic.java
@@ -2,9 +2,20 @@ package com.matskevich.newspringcourse;
 
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
-public class RockMusic implements Music{
+public class RockMusic implements Music {
+    List<String> musicList;
+
+    public RockMusic(List<String> musicList) {
+        this.musicList = musicList;
+        musicList.add("Foo Fighters - Everlong");
+        musicList.add("Nirvana - Something In The Way");
+        musicList.add("LINKIN_PARK - In the End");
+    }
+
     public String getSong() {
-        return "Wind cries Mary";
+        return musicList.get(1);
     }
 }
