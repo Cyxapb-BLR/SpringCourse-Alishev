4f3f5f0cf65fc148ce2232f2e11f95deabceafe7
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-05 14:47:09 -0500

    L11 - Dependency Injection - @Autowired  for 2 arguments in constructor

--- src/main/java/com/matskevich/newspringcourse/ClassicalMusic.java
+++ src/main/java/com/matskevich/newspringcourse/ClassicalMusic.java
@@ -2,7 +2,7 @@ package com.matskevich.newspringcourse;
 
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class ClassicalMusic implements Music {
 
     public String getSong() {
