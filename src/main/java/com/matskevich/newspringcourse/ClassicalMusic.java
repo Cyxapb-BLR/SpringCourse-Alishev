2f3a3f03b67c2489092b238108dcae456ebb22b0
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-05 19:50:09 -0500

    L13 - default @Scope="singleton" for ClassicalMusic, @PostConstruct=init method, @PreDestroy=destroy method

--- src/main/java/com/matskevich/newspringcourse/ClassicalMusic.java
+++ src/main/java/com/matskevich/newspringcourse/ClassicalMusic.java
@@ -3,9 +3,20 @@ package com.matskevich.newspringcourse;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
 public class ClassicalMusic implements Music {
+    @PostConstruct      // init method
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy         // destroy method
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
 
     public String getSong() {
         return "Hungarian Rhapsody";
