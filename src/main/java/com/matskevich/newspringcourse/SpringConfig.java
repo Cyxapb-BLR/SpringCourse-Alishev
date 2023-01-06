29c03105b47c0a9e64e66427247b6f6399247d5d
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-05 20:49:51 -0500

    HW14-4,5,6 -  added rapMusic bean, musicGenreList bean and added it in musicPlayer bean

--- src/main/java/com/matskevich/newspringcourse/SpringConfig.java
+++ src/main/java/com/matskevich/newspringcourse/SpringConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 @PropertySource("classpath:musicPlayer.properties")
 public class SpringConfig {
@@ -19,8 +22,22 @@ public class SpringConfig {
     }
 
     @Bean
+    public RapMusic rapMusic() {
+        return new RapMusic();
+    }
+
+    @Bean
+    public List<Music> musicGenreList() {
+        List<Music> musicList = new ArrayList<Music>();
+        musicList.add(classicalMusic());
+        musicList.add(rockMusic());
+        musicList.add(rapMusic());
+        return musicList;
+    }
+
+    @Bean
     MusicPlayer musicPlayer() {
-        return new MusicPlayer(classicalMusic(), rockMusic());
+        return new MusicPlayer(musicGenreList());
     }
 
     @Bean
