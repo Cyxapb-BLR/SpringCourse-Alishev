db87743a936b0101d639aa655d49ad42a6c1076c
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-05 19:32:37 -0500

    L13 - @Value in MusicPlayer for musicPlayer.properties

--- src/main/java/com/matskevich/newspringcourse/TestSpring.java
+++ src/main/java/com/matskevich/newspringcourse/TestSpring.java
@@ -7,10 +7,12 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
 
-       /* MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();*/
-        Computer computer = context.getBean("computer", Computer.class);
-        System.out.println(computer);
+        /*Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);*/
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
